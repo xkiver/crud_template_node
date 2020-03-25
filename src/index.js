6fce2a3cf826b4f6c371b4afb45960167af4afc0
Francisco Cabezas <francisco.cabezas@xkivertech.cl>
2020-03-25 17:00:17 -0300

    create template

--- src/index.js
+++ src/index.js
@@ -26,31 +26,6 @@ app.get('/', function (req, res) {
     res.send('HELLO WORLD');
 });
 
-app.put('/insert-user', async (req, res) => {
-    const response = await insertUser(req.body.name, req.body.email, req.body.country);
-    res.json(response);
-});
-
-app.patch('/update-user', async (req, res) => {
-    const response = await updateUser(req.body.name, req.body.email, req.body.country);
-    res.json(response);
-});
-
-app.get('/users', async (req, res) => {
-    const response = await getAllUsers();
-    res.json(response);
-});
-
-app.get('/users/:email', async (req, res) => {
-    const response = await getUser(req.params.email);
-    res.json(response);
-});
-
-app.delete('/users/:email', async (req, res) => {
-    const response = await deleteUser(req.params.email);
-    res.json(response);
-});
-
 app.listen(port, function () {
     console.log('Escuchando en el puerto ' + port);
 });
